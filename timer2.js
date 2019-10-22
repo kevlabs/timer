b1f605b363e732677136a0c728a05038f83244c1
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-10-22 15:45:30 -0400

    implemented timer2

---
+++ timer2.js
@@ -0,0 +1,17 @@
+const stdin = process.stdin;
+// don't worry about these next two lines of setup work.
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (keyStr) => {
+  const keyCode = +keyStr.charCodeAt();
+
+  if (keyCode === 3) {
+    process.exit();
+    return;
+  }
+
+  const delay = keyCode === 98 ? 0 : keyCode >= 49 && keyCode <= 57 ? keyCode - 48 : null;
+  if (delay !== null) setTimeout(() => process.stdout.write('beep\n'), delay * 1000);
+
+});

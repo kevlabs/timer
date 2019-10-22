70c1738b5372978221514fb6c770771289989e1b
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-10-22 13:58:16 -0400

    refactored timer1

--- timer1.js
+++ timer1.js
@@ -1,4 +1,4 @@
 const args = process.argv.slice(2);
-const numbers = new Set(args.reduce((numbers, arg) => (+arg > 0 && numbers.concat(+arg)) || numbers, []));
+const numbers = new Set(args.filter(arg => +arg > 0));
 // numbers.forEach(num => setTimeout(() => process.stdout.write('beep\n'), num * 1000));
 numbers.forEach(num => setTimeout(() => process.stdout.write('\x07'), num * 1000));
